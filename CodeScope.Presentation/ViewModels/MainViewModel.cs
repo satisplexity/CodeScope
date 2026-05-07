a656af33daf0a84a6282eb9051769591429e8851
satisplexity <satisplexity@gmail.com>
2026-05-07 22:18:38 +0200

    feat(presentation): implemented MainViewModel

---
+++ CodeScope.Presentation/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CodeScope.Presentation.ViewModels.Base;
+using CodeScope.Presentation.Views;
+using System.Windows.Controls;
+
+namespace CodeScope.Presentation.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private UserControl _currentView;
+
+        public UserControl CurrentView
+        {
+            get => _currentView;
+
+            private set
+            {
+                if (_currentView != value)
+                {
+                    _currentView = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public MainViewModel()
+        {
+            CurrentView = new ProjectsView();
+        }
+    }
+}

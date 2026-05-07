db4641ecd67859d7e9ee0e80ba144558da4402b9
satisplexity <satisplexity@gmail.com>
2026-05-07 21:20:33 +0200

    feat(presentation): register project repository

--- CodeScope.Presentation/App.xaml.cs
+++ CodeScope.Presentation/App.xaml.cs
@@ -1,14 +1,28 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using CodeScope.Application.Projects.Abstractions;
+using CodeScope.Infrastructure.Persistence.Json;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CodeScope.Presentation
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
-    }
+        public IServiceProvider Services { get; private set; } = null!;
 
+        protected override void OnStartup(StartupEventArgs startupArgs)
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            ConfigureServices(services);
+
+            Services = services.BuildServiceProvider();
+
+            base.OnStartup(startupArgs);
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IProjectRepository, JsonProjectRepository>();
+        }
+    }
 }

cafdfed53fe043dbc4231ba47d95593fad4a344d
satisplexity <satisplexity@gmail.com>
2026-05-07 20:49:35 +0200

    feat(domain): add Project entity

---
+++ CodeScope.Domain/Projects/Project.cs
@@ -0,0 +1,23 @@
+﻿namespace CodeScope.Domain.Projects
+{
+    public class Project
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        
+        public string? Description { get; set; }
+
+        public string RootPath { get; set; } = string.Empty;
+
+        public string IconKey {  get; set; } = "Code";
+
+        public DateTime CreatedAt { get; set; }
+
+        public List<string> IncludedExtensions { get; set; } = [];
+
+        public List<string> IgnoredDirectories { get; set; } = [];
+
+        public List<string> IgnoredFiles { get; set; } = [];
+    }
+}

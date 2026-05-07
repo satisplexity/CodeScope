af9030c4e75f8869989d56a8235a4337ebdb0108
satisplexity <satisplexity@gmail.com>
2026-05-07 20:30:08 +0200

    Add presentation layer

---
+++ CodeScope.Presentation/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CodeScope.Presentation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

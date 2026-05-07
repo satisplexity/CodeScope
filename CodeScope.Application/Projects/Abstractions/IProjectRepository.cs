a48a0280fbded5773c84a6d910e5c9e072544fca
satisplexity <satisplexity@gmail.com>
2026-05-07 20:55:14 +0200

    feat(application): add IProjectRepository interface

---
+++ CodeScope.Application/Projects/Abstractions/IProjectRepository.cs
@@ -0,0 +1,17 @@
+﻿using CodeScope.Domain.Projects;
+
+namespace CodeScope.Application.Projects.Abstractions
+{
+    public interface IProjectRepository
+    {
+        Task<List<Project>> GetAllProjectsAsync();
+
+        Task<Project?> GetByIdAsync(Guid id);
+        
+        Task AddAsync(Project project);
+
+        Task DeleteAsync(Guid id);
+
+        Task SaveChangesAsync();
+    }
+}

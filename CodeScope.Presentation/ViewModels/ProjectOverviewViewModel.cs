5e9e74cb16a3e7fe0f212306064a557b0ebe5673
satisplexity <satisplexity@gmail.com>
2026-05-09 21:05:39 +0200

    feat(presentation): add ProjectOverviewViewModel

---
+++ CodeScope.Presentation/ViewModels/ProjectOverviewViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CodeScope.Domain.Projects;
+using CodeScope.Presentation.ViewModels.Base;
+
+namespace CodeScope.Presentation.ViewModels
+{
+    public class ProjectOverviewViewModel : ViewModelBase
+    {
+        public Project CurrentProject { get; }
+
+        public ProjectOverviewViewModel(Project project)
+        {
+            CurrentProject = project;
+        }
+    }
+}

4383a69bf021641631cbe3d2c6e163d7fd471e2a
satisplexity <satisplexity@gmail.com>
2026-05-11 13:13:04 +0200

    feat(application): add ISnapshotRepository interface

---
+++ CodeScope.Application/Projects/Abstractions/ISnapshotRepository.cs
@@ -0,0 +1,11 @@
+﻿using CodeScope.Domain.Projects;
+
+namespace CodeScope.Application.Projects.Abstractions
+{
+    public interface ISnapshotRepository
+    {
+        Task<List<ProjectSnapshot>> GetByProjectIdAsync(Guid projectId);
+
+        Task SaveAsync(ProjectSnapshot snapshot);
+    }
+}

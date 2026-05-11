248d433e203e8b162e83f052b20a424c30ebb6f2
satisplexity <satisplexity@gmail.com>
2026-05-11 20:08:11 +0200

    feat(presentation): implement custom window titlebar

--- CodeScope.Presentation/Views/Windows/MainWindow.xaml.cs
+++ CodeScope.Presentation/Views/Windows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CodeScope.Presentation.ViewModels;
+using System.Windows.Input;
 using System.Windows;
 
 namespace CodeScope.Presentation.Views.Windows
@@ -11,5 +12,29 @@ namespace CodeScope.Presentation.Views.Windows
 
             DataContext = viewModel;
         }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+            => WindowState = WindowState.Minimized;
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+            => SwitchWindowState();
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+            => App.Current.Shutdown();
+
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs mouse)
+        {
+            if (mouse.ClickCount == 2)
+            {
+                SwitchWindowState();
+
+                return;
+            }
+
+            DragMove();
+        }
+
+        private void SwitchWindowState()
+            => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
     }
 }

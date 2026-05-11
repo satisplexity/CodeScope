01f5a059f37a8645cf6c95516b3852019a5fd268
satisplexity <satisplexity@gmail.com>
2026-05-11 13:05:19 +0200

    feat(presentation): add SnapshotsViewModel

---
+++ CodeScope.Presentation/ViewModels/SnapshotsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CodeScope.Presentation.Commands;
+using CodeScope.Presentation.ViewModels.Base;
+
+namespace CodeScope.Presentation.ViewModels
+{
+    public class SnapshotsViewModel : ViewModelBase
+    {
+        public RelayCommand CreateSnapshotCommand { get; }
+
+        public SnapshotsViewModel()
+        {
+
+        }
+    }
+}

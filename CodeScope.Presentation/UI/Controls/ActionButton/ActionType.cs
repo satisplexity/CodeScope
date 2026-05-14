ae360c6aaaaed6b996ca76d8cf2d17e8002defc7
satisplexity <satisplexity@gmail.com>
2026-05-14 19:13:14 +0200

    feat(presentation): add ActionType enum

---
+++ CodeScope.Presentation/UI/Controls/ActionButton/ActionType.cs
@@ -0,0 +1,28 @@
+﻿namespace CodeScope.Presentation.UI.Controls.ActionButton
+{
+    /// <summary>
+    /// Represents the semantic intent of an action.
+    /// </summary>
+    public enum ActionType
+    {
+        /// <summary>
+        /// Neutral action used for standard interactions, navigation or secondary actions.
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Primary action that represents the main action or workflow on the current screen.
+        /// </summary>
+        Primary,
+
+        /// <summary>
+        /// Positive action that related to confirmation, creation or saving data.
+        /// </summary>
+        Success,
+
+        /// <summary>
+        /// Destructive or potentially irreversible action such as deleting or closing without saving.
+        /// </summary>
+        Danger
+    }
+}

857930af6109fd9cb44f0c575c6653cc30189166
satisplexity <satisplexity@gmail.com>
2026-05-17 14:51:27 +0200

    feat(presentation): add MainWindow

---
+++ CodeScope.Presentation/Features/Shell/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace CodeScope.Presentation.Features.Shell
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

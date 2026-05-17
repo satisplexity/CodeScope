4d0248f7e02bf3b5be5ff4fbd2a66c241b3c91e2
satisplexity <satisplexity@gmail.com>
2026-05-17 18:35:33 +0200

    refactor(presentation): move AsyncRelayCommand into foundation

---
+++ CodeScope.Presentation/Framework/Foundation/AsyncRelayCommand.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows.Input;
+
+namespace CodeScope.Presentation.Framework.Foundation
+{
+    public class AsyncRelayCommand : ICommand
+    {
+        private readonly Func<object?, Task> _execute;
+
+        private readonly Predicate<object?>? _canExecute;
+
+        private bool _isExecuting;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public AsyncRelayCommand(Func<Task> execute, Func<bool>? canExecute = null)
+        {
+            if (_execute is null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            _execute = _ => execute();
+
+            _canExecute = canExecute is null
+                ? null
+                : _ => canExecute();
+        }
+
+        public AsyncRelayCommand(Func<object?, Task> execute, Predicate<object?>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Determines whether the command can execute and is not already running.
+        /// </summary>
+        public bool CanExecute(object? parameter) =>
+            !_isExecuting && (_canExecute?.Invoke(parameter) ?? true);
+
+        public async void Execute(object? parameter)
+        {
+            if (!CanExecute(parameter))
+            {
+                return;
+            }
+
+            try
+            {
+                _isExecuting = true;
+                RaiseCanExecuteChanged();
+                await _execute(parameter);
+            }
+            finally
+            {
+                _isExecuting = false;
+                RaiseCanExecuteChanged();
+            }
+        }
+
+        public void RaiseCanExecuteChanged() =>
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}

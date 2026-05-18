af2306d2e069dad1a76ec5343997626afb1e5b6b
satisplexity <satisplexity@gmail.com>
2026-05-18 15:33:01 +0200

    feat(presentation): add create project view

---
+++ CodeScope.Presentation/Features/CreateProject/CreateProjectView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CodeScope.Presentation.Features.CreateProject
+{
+    public partial class CreateProjectView : UserControl
+    {
+        public CreateProjectView()
+        {
+            InitializeComponent();
+        }
+    }
+}

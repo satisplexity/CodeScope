6ad308cd5ee0ad03c3088fa038b654cfe0327927
satisplexity <satisplexity@gmail.com>
2026-05-18 15:38:16 +0200

    feat(presentation): add project workspace view

---
+++ CodeScope.Presentation/Features/ProjectWorkspace/ProjectWorkspaceView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CodeScope.Presentation.Features.ProjectWorkspace
+{
+    public partial class ProjectWorkspaceView : UserControl
+    {
+        public ProjectWorkspaceView()
+        {
+            InitializeComponent();
+        }
+    }
+}

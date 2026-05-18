f08025ae2c0b68e8a8005c444b48ea4a96663cdd
satisplexity <satisplexity@gmail.com>
2026-05-18 15:31:06 +0200

    feat(presentation): add archive view

---
+++ CodeScope.Presentation/Features/Archive/ArchiveView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CodeScope.Presentation.Features.Archive
+{
+    public partial class ArchiveView : UserControl
+    {
+        public ArchiveView()
+        {
+            InitializeComponent();
+        }
+    }
+}

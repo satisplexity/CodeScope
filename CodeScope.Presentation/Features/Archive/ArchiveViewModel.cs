38eff507695a769afe63500f0418e2ce95085116
satisplexity <satisplexity@gmail.com>
2026-05-18 16:07:49 +0200

    feat(presentation): add archive vm

---
+++ CodeScope.Presentation/Features/Archive/ArchiveViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CodeScope.Presentation.Framework.Foundation;
+
+namespace CodeScope.Presentation.Features.Archive
+{
+    public sealed class ArchiveViewModel : ViewModelBase
+    {
+
+    }
+}

c53c7866cc8389d3b6440d9fb7f21043abb4ede5
satisplexity <satisplexity@gmail.com>
2026-05-18 16:25:12 +0200

    feat(presentation): implement NavigationStore

---
+++ CodeScope.Presentation/Framework/Navigation/NavigationStore.cs
@@ -0,0 +1,15 @@
+﻿using CodeScope.Presentation.Framework.Foundation;
+
+namespace CodeScope.Presentation.Framework.Navigation
+{
+    public sealed class NavigationStore : ObservableObject
+    {
+        private ViewModelBase? _currentViewModel;
+
+        public ViewModelBase? CurrentViewModel
+        {
+            get => _currentViewModel;
+            set => SetProperty(ref _currentViewModel, value);
+        }
+    }
+}

698f658cb4efb462bc7ba66990caa6265e026d66
satisplexity <satisplexity@gmail.com>
2026-05-19 01:33:05 +0200

    feat(presentation): implement backward navigation from archive

--- CodeScope.Presentation/Features/Archive/ArchiveViewModel.cs
+++ CodeScope.Presentation/Features/Archive/ArchiveViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using CodeScope.Presentation.Framework.Foundation;
+using CodeScope.Presentation.Framework.Navigation.Abstractions;
 
 namespace CodeScope.Presentation.Features.Archive
 {
     public sealed class ArchiveViewModel : ViewModelBase
     {
+        public RelayCommand GoBackCommand { get; }
 
+        public ArchiveViewModel(IRootNavigationService navigation)
+        {
+            GoBackCommand = new(navigation.GoBack);
+        }
     }
 }

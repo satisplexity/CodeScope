dc86ced8f3010941cc3d4e50960966e02b5e7ec5
satisplexity <satisplexity@gmail.com>
2026-05-18 23:33:40 +0200

    feat(presentation): add ProjectWorkspaceNavigationService

---
+++ CodeScope.Presentation/Framework/Navigation/ProjectWorkspace/ProjectWorkspaceNavigationService.cs
@@ -0,0 +1,13 @@
+﻿using CodeScope.Presentation.Framework.Navigation.Abstractions;
+
+namespace CodeScope.Presentation.Framework.Navigation.ProjectWorkspace
+{
+    public class ProjectWorkspaceNavigationService : NavigationService, IProjectWorkspaceNavigationService
+    {
+        public ProjectWorkspaceNavigationService(ProjectWorkspaceNavigationStore store, IServiceProvider serviceProvider)
+            : base(store, serviceProvider)
+        {
+            
+        }
+    }
+}

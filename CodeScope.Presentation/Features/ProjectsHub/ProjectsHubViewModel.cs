2d3d6e573e7a237046c56f0e4d1dad085e040dc8
satisplexity <satisplexity@gmail.com>
2026-05-18 16:09:25 +0200

    feat(presentation): add projects hub vm

---
+++ CodeScope.Presentation/Features/ProjectsHub/ProjectsHubViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CodeScope.Presentation.Framework.Foundation;
+
+namespace CodeScope.Presentation.Features.ProjectsHub
+{
+    public class ProjectsHubViewModel : ViewModelBase
+    {
+
+    }
+}

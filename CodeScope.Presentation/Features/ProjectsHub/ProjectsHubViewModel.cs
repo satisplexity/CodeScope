76fb0905cbfeba41ad441f15518e3d9d104e607b
satisplexity <satisplexity@gmail.com>
2026-05-19 00:59:38 +0200

    feat(presentation): implement ProjectsHubViewModel

--- CodeScope.Presentation/Features/ProjectsHub/ProjectsHubViewModel.cs
+++ CodeScope.Presentation/Features/ProjectsHub/ProjectsHubViewModel.cs
@@ -1,9 +1,32 @@
-﻿using CodeScope.Presentation.Framework.Foundation;
+﻿using CodeScope.Presentation.Framework.Navigation.Abstractions;
+using CodeScope.Presentation.Features.ProjectWorkspace;
+using CodeScope.Presentation.Features.CreateProject;
+using CodeScope.Presentation.Framework.Foundation;
+using CodeScope.Presentation.Features.Settings;
+using CodeScope.Presentation.Features.Archive;
+using System.Windows;
 
 namespace CodeScope.Presentation.Features.ProjectsHub
 {
     public sealed class ProjectsHubViewModel : ViewModelBase
     {
+        public RelayCommand OpenArchiveCommand { get; }
+        public RelayCommand OpenSettingsCommand { get; }
+        public RelayCommand? ArchiveProjectCommand { get; }
+        public RelayCommand OpenCreateProjectCommand { get; }
+        public RelayCommand OpenProjectWorkspaceCommand { get; }
 
+        public ProjectsHubViewModel(IRootNavigationService rootNavigationService)
+        {
+            OpenArchiveCommand = new(rootNavigationService.NavigateTo<ArchiveViewModel>);
+
+            OpenSettingsCommand = new(rootNavigationService.NavigateTo<SettingsViewModel>);
+
+            OpenArchiveCommand = new(() => MessageBox.Show("ARHIVE PROJECT"));
+
+            OpenCreateProjectCommand = new(rootNavigationService.NavigateTo<CreateProjectViewModel>);
+
+            OpenProjectWorkspaceCommand = new(rootNavigationService.NavigateTo<ProjectWorkspaceViewModel>);
+        }
     }
 }

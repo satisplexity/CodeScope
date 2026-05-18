0c1010291fcba6c8ff9f7b8294abeb66e06157e9
satisplexity <satisplexity@gmail.com>
2026-05-19 01:58:17 +0200

    feat(presentation): implement navigation from workspace to hub

--- CodeScope.Presentation/Features/ProjectWorkspace/ProjectWorkspaceViewModel.cs
+++ CodeScope.Presentation/Features/ProjectWorkspace/ProjectWorkspaceViewModel.cs
@@ -1,9 +1,16 @@
-﻿using CodeScope.Presentation.Framework.Foundation;
+﻿using CodeScope.Presentation.Features.ProjectsHub;
+using CodeScope.Presentation.Framework.Foundation;
+using CodeScope.Presentation.Framework.Navigation.Abstractions;
 
 namespace CodeScope.Presentation.Features.ProjectWorkspace
 {
     public sealed class ProjectWorkspaceViewModel : ViewModelBase
     {
-
+        public RelayCommand OpenProjectHubCommand { get; }
+        
+        public ProjectWorkspaceViewModel(IRootNavigationService navigation)
+        {
+            OpenProjectHubCommand = new(navigation.NavigateTo<ProjectsHubViewModel>);
+        }
     }
 }

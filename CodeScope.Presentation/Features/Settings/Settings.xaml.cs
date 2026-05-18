ab5f28fbbf35415aced7dfc4af80d356805f1414
satisplexity <satisplexity@gmail.com>
2026-05-18 15:29:41 +0200

    feat(presentation): add settings view

---
+++ CodeScope.Presentation/Features/Settings/Settings.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CodeScope.Presentation.Features.Settings
+{
+    public partial class Settings : UserControl
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}

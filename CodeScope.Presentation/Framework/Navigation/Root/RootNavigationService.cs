5e4045aedd8c2b7073590723f2ba6015587da9cd
satisplexity <satisplexity@gmail.com>
2026-05-18 23:31:39 +0200

    feat(presentation): add RootNavigationService

---
+++ CodeScope.Presentation/Framework/Navigation/Root/RootNavigationService.cs
@@ -0,0 +1,14 @@
+﻿using CodeScope.Presentation.Framework.Navigation.Abstractions;
+
+namespace CodeScope.Presentation.Framework.Navigation.Root
+{
+    public sealed class RootNavigationService
+        : NavigationService, IRootNavigationService
+    {
+        public RootNavigationService(RootNavigationStore store, IServiceProvider serviceProvider)
+            : base(store, serviceProvider)
+        {
+
+        }
+    }
+}

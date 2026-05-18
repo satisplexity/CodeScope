d0b877c6de38dbaf3c9e612e4107dedd9a802e01
satisplexity <satisplexity@gmail.com>
2026-05-18 15:36:57 +0200

    feat(presentation): add projects hub view

---
+++ CodeScope.Presentation/Features/ProjectsHub/ProjectsHubView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CodeScope.Presentation.Features.ProjectsHub
+{
+    public partial class ProjectsHubView : UserControl
+    {
+        public ProjectsHubView()
+        {
+            InitializeComponent();
+        }
+    }
+}

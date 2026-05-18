942135a92cc2233884e006ffe7e85b7d19372af5
satisplexity <satisplexity@gmail.com>
2026-05-18 16:22:30 +0200

    feat(presentation): implement INavigationService

---
+++ CodeScope.Presentation/Framework/Navigation/INavigationService.cs
@@ -0,0 +1,19 @@
+﻿using CodeScope.Presentation.Framework.Foundation;
+
+namespace CodeScope.Presentation.Framework.Navigation
+{
+    public interface INavigationService
+    {
+        ViewModelBase CurrentViewModel { get; }
+
+        void NavigateTo<TViewModel>()
+            where TViewModel : ViewModelBase;
+
+        void NavigateTo<TViewModel, TParameter>(TParameter parameter)
+            where TViewModel : ViewModelBase;
+
+        bool CanGoBack { get; }
+
+        void GoBack();
+    }
+}

c6fdb5b20d84c18fbf4469dffb7e3255a50f158b
satisplexity <satisplexity@gmail.com>
2026-05-18 14:55:34 +0200

    feat(presentation): create main-view-model

---
+++ CodeScope.Presentation/Features/Shell/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CodeScope.Presentation.Framework.Foundation;
+
+namespace CodeScope.Presentation.Features.Shell
+{
+    public sealed class MainViewModel : ViewModelBase
+    {
+        private ViewModelBase _currentViewModel;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set => SetProperty(ref _currentViewModel, value);
+        }
+
+        public MainViewModel()
+        {
+
+        }
+    }
+}

dad94d5143c85e3b4b7ef87236f1f966b037625c
satisplexity <satisplexity@gmail.com>
2026-05-18 23:35:36 +0200

    feat(presentation): add CreateProjectNavigationService

---
+++ CodeScope.Presentation/Framework/Navigation/CreateProject/CreateProjectNavigationService.cs
@@ -0,0 +1,13 @@
+﻿using CodeScope.Presentation.Framework.Navigation.Abstractions;
+
+namespace CodeScope.Presentation.Framework.Navigation.CreateProject
+{
+    public class CreateProjectNavigationService : NavigationService, ICreateProjectNavigationService
+    {
+        public CreateProjectNavigationService(CreateProjectNavigationStore store, IServiceProvider serviceProvider)
+            : base(store, serviceProvider)
+        {
+
+        }
+    }
+}

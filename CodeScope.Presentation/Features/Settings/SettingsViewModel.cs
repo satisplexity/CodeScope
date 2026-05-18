a0f68a666ebe827e5d8a281b0341a355d72717bf
satisplexity <satisplexity@gmail.com>
2026-05-19 01:25:15 +0200

    feat(presentation): implement backward navigation from settings

--- CodeScope.Presentation/Features/Settings/SettingsViewModel.cs
+++ CodeScope.Presentation/Features/Settings/SettingsViewModel.cs
@@ -1,9 +1,15 @@
 ﻿using CodeScope.Presentation.Framework.Foundation;
+using CodeScope.Presentation.Framework.Navigation.Abstractions;
 
 namespace CodeScope.Presentation.Features.Settings
 {
     public sealed class SettingsViewModel : ViewModelBase
-    { 
-
+    {
+        public RelayCommand GoBackCommand { get; }
+        
+        public SettingsViewModel(IRootNavigationService navigation)
+        {
+            GoBackCommand = new(navigation.GoBack);
+        }
     }
 }

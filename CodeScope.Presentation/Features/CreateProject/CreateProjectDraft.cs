adfd3f6f5f1e9ae17e07c622f8af624345c29bfd
satisplexity <satisplexity@gmail.com>
2026-05-19 02:49:55 +0200

    feat(presentation): implement create project draft

---
+++ CodeScope.Presentation/Features/CreateProject/CreateProjectDraft.cs
@@ -0,0 +1,25 @@
+﻿using CodeScope.Presentation.Framework.Foundation;
+using System.Collections.ObjectModel;
+
+namespace CodeScope.Presentation.Features.CreateProject
+{
+    public class CreateProjectDraft : ObservableObject
+    {
+        public string RootPath { get; set; } = string.Empty;
+        public string ProjectName { get; set; } = string.Empty;
+
+        public string? Description { get; set;  } = string.Empty;
+
+
+        public ObservableCollection<string> IncludedExtensions { get; } = [];
+        public ObservableCollection<string> IgnoredDirectories { get; } = [];
+        public ObservableCollection<string> IgnoredFiles { get; } = [];
+
+        public bool CreateSnapshotOnAppOpen { get; set; }
+        public bool AutoSnapshotsEnabled { get; set; }
+
+        public string ColorKey { get; set; } = "#Default";
+        public string IconKey { get; set; } = "Code";
+
+    }
+}

6ef3a6314cee3eae8ac9c2db4379cab7284c6b60
satisplexity <satisplexity@gmail.com>
2026-05-19 02:55:06 +0200

    feat(presentation): create analyser settings step vm

---
+++ CodeScope.Presentation/Features/CreateProject/Steps/AnalyzerSettingsStep/AnalyzerSettingsStepViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CodeScope.Presentation.Framework.Foundation;
+
+namespace CodeScope.Presentation.Features.CreateProject.Steps.AnalyzerSettingsStep
+{
+    public class AnalyzerSettingsStepViewModel : ViewModelBase
+    {
+    }
+}

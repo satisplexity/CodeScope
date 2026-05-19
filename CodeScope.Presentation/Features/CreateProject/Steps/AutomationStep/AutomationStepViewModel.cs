bc6df641e883d7495714bcf9d8dc4687b50205f5
satisplexity <satisplexity@gmail.com>
2026-05-19 02:56:00 +0200

    feat(presentation): create automation step vm

---
+++ CodeScope.Presentation/Features/CreateProject/Steps/AutomationStep/AutomationStepViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CodeScope.Presentation.Framework.Foundation;
+
+namespace CodeScope.Presentation.Features.CreateProject.Steps.AutomationStep
+{
+    public class AutomationStepViewModel : ViewModelBase
+    {
+    }
+}

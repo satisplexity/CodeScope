86516c2c007fffeb7660e1e576137f780d8ce4d2
satisplexity <satisplexity@gmail.com>
2026-05-19 02:59:34 +0200

    feat(presentation): create anlyzer settings step view

---
+++ CodeScope.Presentation/Features/CreateProject/Steps/AnalyzerSettingsStep/AnalyzerSettingsStepView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CodeScope.Presentation.Features.CreateProject.Steps.AnalyzerSettingsStep
+{
+    /// <summary>
+    /// Interaction logic for AnalyzerSettingsStepView.xaml
+    /// </summary>
+    public partial class AnalyzerSettingsStepView : UserControl
+    {
+        public AnalyzerSettingsStepView()
+        {
+            InitializeComponent();
+        }
+    }
+}

d8b3ea275531afe63e337041f25da83fd0f92a04
satisplexity <satisplexity@gmail.com>
2026-05-19 03:15:18 +0200

    feat(presentation): implement navigation logic in create project

--- CodeScope.Presentation/Features/CreateProject/CreateProjectViewModel.cs
+++ CodeScope.Presentation/Features/CreateProject/CreateProjectViewModel.cs
@@ -1,4 +1,8 @@
-﻿using CodeScope.Presentation.Framework.Foundation;
+﻿using CodeScope.Presentation.Features.CreateProject.Steps.GeneralInfoStep;
+using CodeScope.Presentation.Features.CreateProject.Steps.AnalyzerSettingsStep;
+using CodeScope.Presentation.Features.CreateProject.Steps.AutomationStep;
+using CodeScope.Presentation.Features.CreateProject.Steps.CustomizationStep;
+using CodeScope.Presentation.Framework.Foundation;
 using CodeScope.Presentation.Framework.Navigation.Abstractions;
 
 namespace CodeScope.Presentation.Features.CreateProject
@@ -6,10 +10,39 @@ namespace CodeScope.Presentation.Features.CreateProject
     public sealed class CreateProjectViewModel : ViewModelBase
     {
         public RelayCommand GoBackCommand { get; }
-        
+
+        private ViewModelBase _currentStep;
+        public ViewModelBase CurrentStep
+        {
+            get => _currentStep;
+            set => SetProperty(ref _currentStep, value);
+        }
+
+        private int _currentStepIndex = 0;
+
+        private readonly ViewModelBase[] _steps;
+
         public CreateProjectViewModel(IRootNavigationService navigation)
         {
             GoBackCommand = new(navigation.GoBack);
+
+            _steps = new ViewModelBase[]
+            {
+                new GeneralInfoStepViewModel(),
+                new AnalyzerSettingsStepViewModel(),
+                new AutomationStepViewModel(),
+                new CustomizationStepViewModel()
+            };
+        }
+
+        public void GoToNextStep()
+        {
+            CurrentStep = _steps[++_currentStepIndex];
+        }
+
+        public void GoToPreviousStep()
+        {
+            CurrentStep = _steps[--_currentStepIndex];
         }
     }
 }

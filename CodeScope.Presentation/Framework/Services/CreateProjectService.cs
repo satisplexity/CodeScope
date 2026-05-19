2614f53e1edbd97c9496b4b60435c93bf0511db4
satisplexity <satisplexity@gmail.com>
2026-05-19 20:51:09 +0200

    feat(presentation): implement create-project service

---
+++ CodeScope.Presentation/Framework/Services/CreateProjectService.cs
@@ -0,0 +1,36 @@
+﻿using CodeScope.Application.Projects.Abstractions;
+using CodeScope.Domain.Projects;
+using CodeScope.Presentation.Features.CreateProject;
+
+namespace CodeScope.Presentation.Framework.Services
+{
+    public sealed class CreateProjectService
+    {
+        private readonly IProjectRepository _repository;
+
+        public CreateProjectService(IProjectRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task ExecuteAsync(CreateProjectDraft draft)
+        {
+            Project project = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = draft.Name,
+                Description = draft.Description,
+                RootPath = draft.RootPath,
+                CreatedAt = DateTime.UtcNow,
+                ColotKey = draft.ColorKey,
+                IconKey = draft.IconKey,
+                IgnoredDirectories = draft.IgnoredDirectories.ToList(),
+                IncludedExtensions = draft.IncludedExtensions.ToList(),
+                IgnoredFiles = draft.IgnoredFiles.ToList(),
+            };
+
+            await _repository.AddAsync(project);
+            await _repository.SaveChangesAsync();
+        }
+    }
+}

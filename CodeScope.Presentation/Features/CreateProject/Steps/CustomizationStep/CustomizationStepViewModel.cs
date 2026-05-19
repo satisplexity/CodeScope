0eb55332d21e94edf08781a44075c099ff67c23d
satisplexity <satisplexity@gmail.com>
2026-05-19 02:56:51 +0200

    feat(presentation): create customization step vm

---
+++ CodeScope.Presentation/Features/CreateProject/Steps/CustomizationStep/CustomizationStepViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CodeScope.Presentation.Framework.Foundation;
+
+namespace CodeScope.Presentation.Features.CreateProject.Steps.CustomizationStep
+{
+    public class CustomizationStepViewModel : ViewModelBase
+    {
+    }
+}

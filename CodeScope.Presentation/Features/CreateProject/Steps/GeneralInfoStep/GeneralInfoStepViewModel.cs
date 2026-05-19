07e9cb94a67be6d92f9df1f64eff52e6df1432af
satisplexity <satisplexity@gmail.com>
2026-05-19 02:54:02 +0200

    feat(presentation): create general info stem vm

---
+++ CodeScope.Presentation/Features/CreateProject/Steps/GeneralInfoStep/GeneralInfoStepViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CodeScope.Presentation.Framework.Foundation;
+
+namespace CodeScope.Presentation.Features.CreateProject.Steps.GeneralInfoStep
+{
+    public class GeneralInfoStepViewModel : ViewModelBase
+    {
+    }
+}

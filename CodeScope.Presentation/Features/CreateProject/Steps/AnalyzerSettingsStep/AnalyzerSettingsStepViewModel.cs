8e52dda45b528519c9b221a4a72f362562849711
satisplexity <satisplexity@gmail.com>
2026-05-19 05:45:13 +0200

    feat(presentation): move part of functions to analyzer step

--- CodeScope.Presentation/Features/CreateProject/Steps/AnalyzerSettingsStep/AnalyzerSettingsStepViewModel.cs
+++ CodeScope.Presentation/Features/CreateProject/Steps/AnalyzerSettingsStep/AnalyzerSettingsStepViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CodeScope.Presentation.Framework.Foundation;
+using Microsoft.Win32;
 
 namespace CodeScope.Presentation.Features.CreateProject.Steps.AnalyzerSettingsStep
 {
@@ -8,14 +9,143 @@ namespace CodeScope.Presentation.Features.CreateProject.Steps.AnalyzerSettingsSt
 
         public RelayCommand GoToGeneralInfoStepCommand { get; }
 
-        private readonly CreateProjectDraft _draft;
+        public RelayCommand AddExtensionCommand { get; }
+
+        public RelayCommand RemoveExtensionCommand { get; }
+
+        public RelayCommand AddIngnoredDirectoryCommand { get; }
+
+        public RelayCommand RemoveIgnoredDirectoryCommand { get; }
+
+        public RelayCommand AddIgnoredFileCommand { get; }
+        public RelayCommand RemoveIgnoredFileCommand { get; }
+
+        public RelayCommand SelectIgnoredDirectoryCommand { get; }
+        public RelayCommand SelectIgnoredFileCommand { get; }
+
+        private string _newExtension = string.Empty;
+        public string NewExtension
+        {
+            get => _newExtension;
+            set => SetProperty(ref _newExtension, value);
+        }
+
+        private string _newIgnoredDirectory = string.Empty;
+
+        public string NewIngnoredDirectory
+        {
+            get => _newIgnoredDirectory;
+            set => SetProperty(ref _newIgnoredDirectory, value);
+        }
+
+        private string _newIgnoredFile = string.Empty;
+
+        public string NewIgnoredFile
+        {
+            get => _newIgnoredFile;
+            set => SetProperty(ref _newIgnoredFile, value);
+        }
+
+        public CreateProjectDraft Draft { get; }
 
         public AnalyzerSettingsStepViewModel(CreateProjectDraft draft, Action goToNextStepAction, Action goToPreviosStepAction)
         {
-            _draft = draft;
+            Draft = draft;
 
             GoToAutomationStepCommand = new(goToNextStepAction);
             GoToGeneralInfoStepCommand = new(goToPreviosStepAction);
+
+            AddExtensionCommand = new(AddExtension);
+            RemoveExtensionCommand = new(RemoveExtension);
+
+            AddIngnoredDirectoryCommand = new(AddIngnoredDirectory);
+            RemoveIgnoredDirectoryCommand = new(RemoveIgnoredDirectory);
+
+            AddIgnoredFileCommand = new(AddIgnoredFile);
+            RemoveIgnoredFileCommand = new(RemoveIngoredFile);
+
+            SelectIgnoredDirectoryCommand = new(SelectIgnoredDirectory);
+            SelectIgnoredFileCommand = new(SelectIgnoredFile);
+        }
+
+        private void AddExtension()
+        {
+            string extension = NewExtension.Trim();
+
+            if (string.IsNullOrWhiteSpace(extension))
+                return;
+
+            if (!extension.StartsWith("."))
+                extension = "." + extension;
+
+            if (!Draft.IncludedExtensions.Contains(extension))
+                Draft.IncludedExtensions.Add(extension);
+
+            NewExtension = string.Empty;
+        }
+
+        private void RemoveExtension(object? parameter)
+        {
+            if (parameter is string extension)
+                Draft.IncludedExtensions.Remove(extension);
+        }
+
+        private void AddIngnoredDirectory()
+        {
+            string directory = NewIngnoredDirectory.Trim();
+
+            if (string.IsNullOrWhiteSpace(directory))
+                return;
+
+            if (!Draft.IgnoredDirectories.Contains(directory))
+                Draft.IgnoredDirectories.Add(directory);
+
+            NewIngnoredDirectory = string.Empty;
+        }
+
+        private void RemoveIgnoredDirectory(object? parameter)
+        {
+            if (parameter is string directory)
+                Draft.IgnoredDirectories.Remove(directory);
+        }
+
+        private void AddIgnoredFile()
+        {
+            string file = NewIgnoredFile.Trim();
+
+            if (string.IsNullOrWhiteSpace(file))
+                return;
+
+            if (!Draft.IgnoredFiles.Contains(file))
+                Draft.IgnoredFiles.Add(file);
+        }
+
+        private void RemoveIngoredFile(object? parameter)
+        {
+            if (parameter is string file)
+                Draft.IgnoredFiles.Remove(file);
+        }
+
+        private void SelectIgnoredDirectory()
+        {
+            OpenFolderDialog dialog = new OpenFolderDialog();
+            dialog.Title = "Select ignored directory";
+
+            if (dialog.ShowDialog() == true)
+            {
+                NewIngnoredDirectory = dialog.FolderName;
+            }
+        }
+
+        private void SelectIgnoredFile()
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select ignored file";
+
+            if (dialog.ShowDialog() == true)
+            {
+                NewIgnoredFile = dialog.FileName;
+            }
         }
     }
 }
--- CodeScope.Presentation/Features/CreateProject/Steps/GeneralInfoStep/GeneralInfoStepViewModel.cs
+++ CodeScope.Presentation/Features/CreateProject/Steps/GeneralInfoStep/GeneralInfoStepViewModel.cs
@@ -10,19 +10,6 @@ namespace CodeScope.Presentation.Features.CreateProject.Steps.GeneralInfoStep
 
         public RelayCommand SelectRootPathCommand { get; }
 
-        public RelayCommand AddExtensionCommand { get; }
-
-        public RelayCommand RemoveExtensionCommand { get; }
-
-        public RelayCommand AddIngnoredDirectoryCommand { get; }
-
-        public RelayCommand RemoveIgnoredDirectoryCommand { get; }
-
-        public RelayCommand AddIgnoredFileCommand { get; }
-        public RelayCommand RemoveIgnoredFileCommand { get; }
-
-        public RelayCommand SelectIgnoredDirectoryCommand { get; }
-        public RelayCommand SelectIgnoredFileCommand { get; }
 
         public string ProjectName
         {
@@ -60,28 +47,6 @@ namespace CodeScope.Presentation.Features.CreateProject.Steps.GeneralInfoStep
             }
         }
 
-        private string _newExtension = string.Empty;
-        public string NewExtension
-        {
-            get => _newExtension;
-            set => SetProperty(ref _newExtension, value);
-        }
-
-        private string _newIgnoredDirectory = string.Empty;
-
-        public string NewIngnoredDirectory
-        {
-            get => _newIgnoredDirectory;
-            set => SetProperty(ref _newIgnoredDirectory, value);
-        }
-
-        private string _newIgnoredFile = string.Empty;
-
-        public string NewIgnoredFile
-        {
-            get => _newIgnoredFile;
-            set => SetProperty(ref _newIgnoredFile, value);
-        }
 
         private bool _projectNameHasBeenChanged = false;
 
@@ -94,18 +59,6 @@ namespace CodeScope.Presentation.Features.CreateProject.Steps.GeneralInfoStep
 
             GoToAnalyzerSettingStepCommand = new(goToNextStepAction);
             SelectRootPathCommand = new(SelectRootPath);
-
-            AddExtensionCommand = new(AddExtension);
-            RemoveExtensionCommand = new(RemoveExtension);
-
-            AddIngnoredDirectoryCommand = new(AddIngnoredDirectory);
-            RemoveIgnoredDirectoryCommand = new(RemoveIgnoredDirectory);
-
-            AddIgnoredFileCommand = new(AddIgnoredFile);
-            RemoveIgnoredFileCommand = new(RemoveIngoredFile);
-
-            SelectIgnoredDirectoryCommand = new(SelectIgnoredDirectory);
-            SelectIgnoredFileCommand = new(SelectIgnoredFile);
         }
 
         private void SelectRootPath()
@@ -117,86 +70,6 @@ namespace CodeScope.Presentation.Features.CreateProject.Steps.GeneralInfoStep
             if(dialog.ShowDialog() == true)
             {
                 RootPath = dialog.FolderName;
-            }
-        }
-
-        private void AddExtension()
-        {
-            string extension = NewExtension.Trim();
-
-            if (string.IsNullOrWhiteSpace(extension))
-                return;
-
-            if (!extension.StartsWith("."))
-                extension = "." + extension;
-
-            if(!Draft.IncludedExtensions.Contains(extension))
-                Draft.IncludedExtensions.Add(extension);
-
-            NewExtension = string.Empty;
-        }
-
-        private void RemoveExtension(object? parameter)
-        {
-            if(parameter is string extension)
-                Draft.IncludedExtensions.Remove(extension);
-        }
-
-        private void AddIngnoredDirectory()
-        {
-            string directory = NewIngnoredDirectory.Trim();
-
-            if (string.IsNullOrWhiteSpace(directory))
-                return;
-
-            if(!Draft.IgnoredDirectories.Contains(directory))
-                Draft.IgnoredDirectories.Add(directory);
-
-            NewIngnoredDirectory = string.Empty;
-        }
-
-        private void RemoveIgnoredDirectory(object? parameter)
-        {
-            if(parameter is string directory)
-                Draft.IgnoredDirectories.Remove(directory);
-        }
-
-        private void AddIgnoredFile()
-        {
-            string file = NewIgnoredFile.Trim();
-
-            if (string.IsNullOrWhiteSpace(file))
-                return;
-
-            if(!Draft.IgnoredFiles.Contains(file))
-                Draft.IgnoredFiles.Add(file);
-        }
-
-        private void RemoveIngoredFile(object? parameter)
-        {
-            if(parameter is string file)
-                Draft.IgnoredFiles.Remove(file);
-        }
-
-        private void SelectIgnoredDirectory()
-        {
-            OpenFolderDialog dialog = new OpenFolderDialog();
-            dialog.Title = "Select ignored directory";
-
-            if (dialog.ShowDialog() == true)
-            {
-                NewIngnoredDirectory = dialog.FolderName;
-            }
-        }
-
-        private void SelectIgnoredFile()
-        {
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Title = "Select ignored file";
-
-            if(dialog.ShowDialog() == true)
-            {
-                NewIgnoredFile = dialog.FileName;
             }
         }
     }

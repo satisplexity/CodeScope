99c41170e5c41e490395a4be7b7c64799f008567
satisplexity <satisplexity@gmail.com>
2026-05-19 03:01:57 +0200

    feat(presentation): create customization step view

---
+++ CodeScope.Presentation/Features/CreateProject/Steps/CustomizationStep/CustomizationStepView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CodeScope.Presentation.Features.CreateProject.Steps.CustomizationStep
+{
+    public partial class CustomizationStepView : UserControl
+    {
+        public CustomizationStepView()
+        {
+            InitializeComponent();
+        }
+    }
+}

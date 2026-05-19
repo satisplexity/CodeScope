ecc1bb55b382fe3d68e522ce73ee6e8474f6e40e
satisplexity <satisplexity@gmail.com>
2026-05-19 02:58:35 +0200

    feat(presentation): create general info step view

---
+++ CodeScope.Presentation/Features/CreateProject/Steps/GeneralInfoStep/GeneralInfoStepView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CodeScope.Presentation.Features.CreateProject.Steps.GeneralInfoStep
+{
+    public partial class GeneralInfoStepView : UserControl
+    {
+        public GeneralInfoStepView()
+        {
+            InitializeComponent();
+        }
+    }
+}

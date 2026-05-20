3c2401d663675269eeda37f1f5875052adc26f07
satisplexity <satisplexity@gmail.com>
2026-05-20 05:11:09 +0200

    feat(archive): add archive vm initialization

--- CodeScope.Presentation/Features/Archive/ArchiveViewModel.cs
+++ CodeScope.Presentation/Features/Archive/ArchiveViewModel.cs
@@ -1,15 +1,35 @@
-﻿using CodeScope.Presentation.Framework.Foundation;
-using CodeScope.Presentation.Framework.Navigation.Abstractions;
+﻿using CodeScope.Presentation.Framework.Navigation.Abstractions;
+using CodeScope.Presentation.Framework.Foundation;
+using CodeScope.Application.Projects.Abstractions;
+using System.Collections.ObjectModel;
+using CodeScope.Domain.Projects;
 
 namespace CodeScope.Presentation.Features.Archive
 {
-    public sealed class ArchiveViewModel : ViewModelBase
+    public sealed class ArchiveViewModel : ViewModelBase, IAsyncInitializable
     {
         public RelayCommand GoBackCommand { get; }
 
-        public ArchiveViewModel(IRootNavigationService navigation)
+        public ObservableCollection<Project> Projects { get; } = [];
+
+        private readonly IProjectRepository _repository;
+
+        public ArchiveViewModel(IRootNavigationService navigation, IProjectRepository repository)
         {
+            _repository = repository;
+
             GoBackCommand = new(navigation.GoBack);
+        }
+
+        public async Task InitializeAsync()
+        {
+            List<Project> projects = await _repository.GetAllProjectsAsync();
+
+            Projects.Clear();
+
+            foreach (Project project in projects)
+                if (!project.IsArchived)
+                    Projects.Add(project);
         }
     }
 }

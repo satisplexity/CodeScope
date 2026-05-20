2bce2bfb070102a2fbac09f923cbc91d9fee020f
satisplexity <satisplexity@gmail.com>
2026-05-20 08:28:43 +0200

    feat(application): add project model

---
+++ CodeScope.Application/Project/Models/ProjectModel.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeScope.Application.Project.Models
+{
+    public class ProjectModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Path { get; set; } = string.Empty;
+
+        public ProjectScannerSettingsModel ScanerSettings { get; set; }
+
+        public string IconKey { get; set; }
+
+        public string ColorKey { get; set; }
+    }
+}
---
+++ CodeScope.Application/Project/Models/ProjectScannerSettingsModel.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeScope.Application.Project.Models
+{
+    public class ProjectScannerSettingsModel
+    {
+        public Guid Id { get; set; }
+        public Guid ProjectId { get; set; }
+
+        public List<string> Extensions { get; set; } = [];
+
+        public List<string> IgnoredFiles { get; set; } = [];
+
+        public List<string> IgnoredDirectories { get; set; } = [];
+    }
+}
